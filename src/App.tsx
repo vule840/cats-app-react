1fe5b19d36c1b8d6bf7532d0867025aa8152bc0c
Tomy v <vule840@gmail.com>
2021-09-21 12:47:25 +0200

    header, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h2>Start</h2>
+      <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
